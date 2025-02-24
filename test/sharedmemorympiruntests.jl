7a2ac3332908ce8639e07094a6aec34f3cbd2b35
James Cook <cookjws@gmail.com>
2025-02-24 18:57:50 +0000

    free win

--- test/sharedmemorympiruntests.jl
+++ test/sharedmemorympiruntests.jl
@@ -77,6 +77,7 @@ using Random, Test, LinearAlgebra
     StructuredStaticCondensation.distributeenumerations!(SCM, sharedrank, sharedsize)
     sleep(rank)
     @test ldiv!(SCM, b; callback=MPICallBack(sharedcomm, sharedrank, sharedsize)) ≈ x
+    MPI.free(win)
   end
 
   for (L, C) in ((3, 2), (4, 2), (16, 4), (5, 7), (128, 64), (256, 128), (1024, 512))
